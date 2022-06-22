f8f4753fb06725fc6eaf718aad000688e58f468e
MiGueL0n <54883648+Miguelonlonlon@users.noreply.github.com>
2022-06-22 03:42:07 +0200

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,37 @@
-'use strict';
+"use strict"
 
-var gulp = require('gulp');
+const babel = require("gulp-babel");
+const del = require("del");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 
-require('require-dir')('./build', {recurse: true});
+let paths = {
+    scss: [ "src/angular-carousel.scss" ],
+    main: [ "src/angular-carousel.js" ],
 
-gulp.task('default', function(){
-	gulp.start('build');
+    dest: "dist/"
+};
+
+gulp.task("carousel.clean", function () {
+    return del([ paths.dest ]);
 });
+
+gulp.task("carousel.sass", function () {
+    return gulp.
+        src(paths.scss).
+        pipe(sass()).
+        pipe(gulp.dest(paths.dest));
+});
+
+gulp.task("carousel.js", function () {
+    return gulp.src(paths.main).
+        pipe(babel({
+            "presets": [ "es2015" ]
+        })).
+        pipe(gulp.dest(paths.dest));
+});
+
+gulp.task("carousel", [ "carousel.sass", "carousel.js" ]);
+
+gulp.task("clean", [ "carousel.clean" ]);
+gulp.task("default", [ "carousel" ]);
